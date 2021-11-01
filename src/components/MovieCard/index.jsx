93eae165c1328e27770cc4f496d59a25044b507e
realonereal <erohinreal@gmail.com>
2021-11-01 13:45:42 +0300

    fix: styles movie card

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -12,34 +12,40 @@ const MovieCard = (props) => {
   };
 
   const styles = {
+    container: {
+      position: "relative",
+      ":hover .MuiPaper-root": {
+        transition: "0.6s ease",
+        opacity: 0.3,
+      },
+    },
     card: {
       width: "200px",
       height: "300px",
       position: "relative",
       bgcolor: "secondary.dark",
-      ":hover": {
-        transition: "0.6s ease",
-        opacity: 0.8,
-      },
-      ":hover .rating": {
-        display: "none",
-      },
     },
     hoverLayer: {
-      width: "inherit",
-      height: "220px",
+      width: "200px",
+      height: "300px",
       position: "absolute",
-      bgcolor: "secondary.dark",
+      left: 0,
+      top: 0,
       opacity: 0,
       textAlign: "center",
-      pt: "60%",
       cursor: "pointer",
       ":hover": {
         transition: " .3s ease",
         opacity: 1,
       },
     },
-    playIcon: { color: "primary.light", fontSize: "60px" },
+    playIcon: {
+      color: "primary.main",
+      fontSize: "60px",
+      position: "absolute",
+      top: "calc(50% - 30px)",
+      left: "calc(50% - 30px)",
+    },
     ratingBox: {
       position: "absolute",
       top: "20px",
@@ -60,31 +66,36 @@ const MovieCard = (props) => {
   };
 
   return (
-    <Card sx={styles.card}>
+    <Box sx={styles.container}>
+      <Card sx={styles.card}>
+        <Box className="rating" sx={styles.ratingBox}>
+          <Typography variant="subtitle1" component="span">
+            {vote_average}
+          </Typography>
+        </Box>
+        <CardMedia
+          component="img"
+          height="220px"
+          image={poster_path}
+          sx={styles.cardImage}
+        />
+        <CardContent sx={{ p: 1 }}>
+          <Typography variant="h6" color="primary.light">
+            {title}
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            sx={styles.genres}
+            color="primary.dark"
+          >
+            {genres}
+          </Typography>
+        </CardContent>
+      </Card>
       <Box sx={styles.hoverLayer} onClick={handleClick}>
         <PlayCircle sx={styles.playIcon} />
       </Box>
-
-      <Box className="rating" sx={styles.ratingBox}>
-        <Typography variant="subtitle1" component="span">
-          {vote_average}
-        </Typography>
-      </Box>
-      <CardMedia
-        component="img"
-        height="220px"
-        image={poster_path}
-        sx={styles.cardImage}
-      />
-      <CardContent sx={{ p: 1 }}>
-        <Typography variant="h6" color="primary.light">
-          {title}
-        </Typography>
-        <Typography variant="subtitle2" sx={styles.genres} color="primary.dark">
-          {genres}
-        </Typography>
-      </CardContent>
-    </Card>
+    </Box>
   );
 };
 
