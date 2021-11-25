ef2210a4c5ef222f51eb1702edef7fc7f47cd79f
realonereal <erohinreal@gmail.com>
2021-11-25 17:51:51 +0300

    add:netlify status badge

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Box, TextField, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
